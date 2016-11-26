cfb0de1b4371cade3ee17f5e585858e1794ee23d
shepherd <heimd4ll@yeah.net>
2016-11-26 16:59:18 +0800

    refactor project

--- crawler.py
+++ crawler.py
@@ -1,16 +1,10 @@
-import htmldownloader
-import htmlparser
-
-
 class Crawler(object):
     def __init__(self):
-        self.url = ''
         self.base_url = ''
-        self.downloader = htmldownloader.HtmlDownLoader(self.url)
-        self.html_cont = self.downloader.download()
-        self.parser = htmlparser.HtmlParser(self.html_cont)
-        self.soup = self.parser.get_soup()
         self.data = []
+
+    def get_url(self):
+        pass
 
     def craw(self):
         pass
--- gjcrawler.py
+++ gjcrawler.py
@@ -1,6 +1,20 @@
+import htmldownloader
+import htmlparser
 from crawler import Crawler
 
 
 class GJCrawler(Crawler):
+    def get_url(self):
+        urls = []
+        return urls
+
     def craw(self):
+        for url in self.get_url():
+            tmp_dict = {}
+            downloader = htmldownloader.HtmlDownLoader(url)
+            html_cont = downloader.download()
+            parser = htmlparser.HtmlParser(html_cont)
+            soup = parser.get_soup()
+            tmp_dict['tmp'] = soup.find_all()
+            self.data.append(tmp_dict)
         return self.data
