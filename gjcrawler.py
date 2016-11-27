cc12ee1396a935a734ba961b7e6090763d21f500
shepherd <heimd4ll@yeah.net>
2016-11-27 11:04:44 +0800

    finish crawling code about ganji

--- gjcrawler.py
+++ gjcrawler.py
@@ -1,3 +1,5 @@
+import re
+
 import htmldownloader
 import htmlparser
 from crawler import Crawler
@@ -5,16 +7,34 @@ from crawler import Crawler
 
 class GJCrawler(Crawler):
     def get_url(self):
+        url = ['http://eerduosi.ganji.com/zpjisuanjiwangluo/o1/', 'http://nmg.ganji.com/zpjisuanjiwangluo/o1/', 'http://baotou.ganji.com/zpjisuanjiwangluo/o1/']
         urls = []
+        for item in url:
+            curr_page = 1
+            max_page = 10
+            while curr_page <= max_page:
+                urls.append(re.sub("o\d", "o%d" % curr_page, item))
+                curr_page += 1
+        print('...got %d pages about job info from ganji' % len(urls))
         return urls
 
     def craw(self):
+        print('crawling job info from ganji')
         for url in self.get_url():
-            tmp_dict = {}
             downloader = htmldownloader.HtmlDownLoader(url)
             html_cont = downloader.download()
             parser = htmlparser.HtmlParser(html_cont)
             soup = parser.get_soup()
-            tmp_dict['tmp'] = soup.find_all()
-            self.data.append(tmp_dict)
+            items = soup.select("dl.list-noimg.job-list")
+            for item in items:
+                tmp_dict = {}
+                tmp_dict['media'] = 'ganji'
+                tmp_dict['jobname'] = item.find("dt").find("a").get_text().strip()
+                tmp_dict['joblink'] = item.find("dt").find("a").get("href")
+                tmp_dict['company'] = item.find("dd", class_="company").find("a").get_text()
+                tmp_dict['location'] = item.find("dd", class_="pay").get_text()
+                tmp_dict['salary'] = 'unknown'
+                self.data.append(tmp_dict)
+        print('...got %d job info items from ganji' % len(self.data))
+        print(self.data)
         return self.data
